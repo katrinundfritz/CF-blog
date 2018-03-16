e8f59be3f69f7b993d5ebf51f45d6a4b665d0046
katrinundfritz <fritzkatrin@gmail.com>
2018-03-16 17:52:50 +0100

    Added category field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311144207) do
+ActiveRecord::Schema.define(version: 20180316164153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180311144207) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
   end
 
 end
