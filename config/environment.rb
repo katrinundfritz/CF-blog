1352b59bf5636ae42e8b14fc96e580b64405e331
katrinundfritz <fritzkatrin@gmail.com>
2018-03-16 18:42:24 +0100

    Fixed bug in navigation bar

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+my_date_formats = { :default => '%d/%m/%Y' }
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(my_date_formats)
+
+ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(my_date_formats)
