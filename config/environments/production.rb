3ed323a4b2f153a819b42e0ad4a01a3064418a33
katrinundfritz <fritzkatrin@gmail.com>
2018-05-07 18:46:23 +0200

    Fix wrong link to heroku project second try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ‘catsareevil.herokuapp.com’ }
+  config.action_mailer.default_url_options = { host: 'cf-example-blog.herokuapp.com' }
 end
-
-config.action_mailer.default_url_options = { host: 'cf-example-blog.herokuapp.com' }
