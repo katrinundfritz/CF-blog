c59de6301c6213152359c344160a11e3cd326c79
katrinundfritz <fritzkatrin@gmail.com>
2018-05-07 19:40:21 +0200

    Update user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,17 @@
-def welcome(user)
+class UserMailer < ApplicationMailer
+  default from: "message@cf-shop.com"
+
+  def contact_form(email, name, message)
+  @message = message
+    mail(from: email,
+         to: 'fritzkatrin@gmail.com',
+         subject: "A new contact form message from #{name}")
+  end
+
+  def welcome(user)
   @appname = "Bike Shop"
   mail(to: user.email,
        subject: "Welcome to #{@appname}!")
+  end
+
 end
